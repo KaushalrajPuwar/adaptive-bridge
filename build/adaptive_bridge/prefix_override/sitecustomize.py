8e693dacdc6363d2bfca241c2b20390c4f59344a
Kaushalraj Puwar <kaushalrajpuwar@gmail.com>
2025-11-12 16:20:56 +0530

    Fix .gitignore for ROS 2; remove tracked build artifacts

--- build/adaptive_bridge/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/kaushalraj/adaptive_bridge_ws/install/adaptive_bridge'
--- build/adaptive_bridge/share/adaptive_bridge/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/kaushalraj/adaptive_bridge_ws/build/adaptive_bridge"
